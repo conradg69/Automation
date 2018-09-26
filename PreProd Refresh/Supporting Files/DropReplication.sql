ce8aff356fa8ca0882fa42c6a4a89cee071fdb67
Gauntlett <Conrad.Gauntlett@wyndhamdn.com>
2018-09-26 21:07:18 +0100

    Minor Update

--- PreProd Refresh/Supporting Files/DropReplication.sql
+++ PreProd Refresh/Supporting Files/DropReplication.sql
@@ -3,3 +3,4 @@ GO
 EXEC sp_dropsubscription @publication = N'pubFusionILTCache', @article = N'all', @subscriber = N'all', @destination_db = N'all'
 GO
 EXEC sp_droppublication @publication = N'pubFusionILTCache'
+GO

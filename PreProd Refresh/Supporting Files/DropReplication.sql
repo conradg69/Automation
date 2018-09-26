dcd10d868da9388fd9c67aaa81a581776f173713
Gauntlett <Conrad.Gauntlett@wyndhamdn.com>
2018-09-26 16:45:13 +0100

    First Commit

---
+++ PreProd Refresh/Supporting Files/DropReplication.sql
@@ -0,0 +1,5 @@
+USE TR4_PRE_PROD
+GO
+EXEC sp_dropsubscription @publication = N'pubFusionILTCache', @article = N'all', @subscriber = N'all', @destination_db = N'all'
+GO
+EXEC sp_droppublication @publication = N'pubFusionILTCache'

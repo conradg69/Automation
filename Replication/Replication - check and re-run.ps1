2a2b0db6f77e75178da8ff934d434938126b9aad
conradg69 <36333522+conradg69@users.noreply.github.com>
2018-10-22 11:32:44 +0100

    Minor update

--- Replication/Replication - check and re-run.ps1
+++ Replication/Replication - check and re-run.ps1
@@ -3,7 +3,7 @@ Import-Module dbatools;
 #$LondonILTServers = 'PH272708', 'PH272808', 'PH272908', 'PH272908', 'PH302108', 'PH302208', 'PH347608', 'PH347708', 'PH347808', 'PH508008', 'PH509608', 'PH521908'
 
 #Get all the Replication Distibution jobs except the StaticAvailability. Restart any idle jobs
-$SQLJobs = Get-DbaAgentJob -SqlInstance CP472808SQL -Verbose | Where {($_.Category -eq "REPL-Distribution" -and $_.Name -notlike "*StaticAvailability*")}
+$SQLJobs = Get-DbaAgentJob -SqlInstance CP472808SQL -Verbose | Where-Object {($_.Category -eq "REPL-Distribution" -and $_.Name -notlike "*StaticAvailability*")}
     foreach($RepJob in $SQLJobs)
     {
         if ($RepJob.CurrentRunStatus -eq "Idle")

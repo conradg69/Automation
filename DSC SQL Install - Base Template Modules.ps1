d7c64fbee3aa4439b72817cab6ca680b72a98e39
conradg69 <36333522+conradg69@users.noreply.github.com>
2018-12-05 20:37:13 +0000

    Create DSC SQL Install - Base Template Modules.ps1

---
+++ DSC SQL Install - Base Template Modules.ps1
@@ -0,0 +1,36 @@
+sl C:\SQLDSCInstall\SQLMofFiles\
+
+Configuration DSCModules
+{
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $PackagePath
+    )
+
+Import-DscResource –ModuleName PSDesiredStateConfiguration
+
+ Node $AllNodes.where{ $_.Role.Contains("SQLENGINE") }.NodeName
+    {
+        Log ParamLog
+        {
+            Message = "Running DSCModules deployment. PackagePath = $PackagePath"
+        }
+
+
+        File CopyDSCModulesToServers 
+        {
+            Ensure = "Present"
+            Type = "Directory"
+            Recurse = $true
+            MatchSource = $true
+            SourcePath = "$PackagePath\Modules"
+            DestinationPath = "C:\Program Files\WindowsPowershell\Modules\"
+        }
+    }
+ }
+
+DSCModules -ConfigurationData C:\SQLDSCInstall\SetupScripts\ServerConfiguration.psd1 -PackagePath "\\DC1\InstallMedia" -verbose 
+
+Start-DscConfiguration -Path C:\SQLDSCInstall\SQLMofFiles\DSCModules -Wait -Force -Verbose
